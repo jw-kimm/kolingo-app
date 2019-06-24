4f371a902e403c6d77998075d71bfd371c8dc9f6
jw-kimm <jwki0071@gmail.com>
2019-06-24 16:28:10 -0400

    adding client side

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+import  Navbar from './components/navbar'
+
+import  User from './components/users'
+// import Routes from 
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar />
+        {/* <Routes /> */}
+        <User />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/users.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// import  User from './components/navbar'
+// import Routes from 
+
+function User() {
+  return (
+    <div className="App">
+        <ul id = 'user-list'>
+            <li> Users </li>
+        </ul>
+    </div>
+  );
+}
+
+export default User;
