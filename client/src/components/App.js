7db9fcd444e4a4daa9b8b5d18330a58069190df3
jw-kimm <jwki0071@gmail.com>
2019-09-15 17:53:25 -0400

    Edit app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,7 +4,7 @@ import '../index.css'
 
 import Splash from './splash/Splash'
 import LessonsList from './lessons/LessonsList'
-import Alphabet from './lessons/Alphabet';
+// import Alphabet from './lessons/Alphabet';
 // import Matching from './lessons/Matching/Matching';
 // import Advanced from './lessons/Advanced/Advanced';
 
@@ -21,7 +21,7 @@ const App = () => (
       <Route exact path="/" component={Splash} />
       <Route exact path="/lessons" component={LessonsList} />
       <Route exact path="/register" component={Register} />
-      <Route exact path="/alphabet" component={Alphabet} />
+      {/* <Route exact path="/alphabet" component={Alphabet} /> */}
       {/* <Route exact path="/matching" component={Matching} />
       <Route exact path="/advanced" component={Advanced} /> */}
       <Route exact path="/profile" component={UserPage} />
