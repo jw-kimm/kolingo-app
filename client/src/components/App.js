961417b517f7cb9957cf9f7543c02cd31e2e95ef
jw-kimm <jwki0071@gmail.com>
2019-09-15 18:07:30 -0400

    Edit app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,11 +4,10 @@ import '../index.css'
 
 import Splash from './splash/Splash'
 import LessonsList from './lessons/LessonsList'
-// import Alphabet from './lessons/Alphabet';
-// import Matching from './lessons/Matching/Matching';
-// import Advanced from './lessons/Advanced/Advanced';
+import Alphabet from './lessons/Alphabet';
+import Matching from './lessons/Matching/Matching';
+import Advanced from './lessons/Advanced/Advanced';
 
-// import Matching from './lessons/Matching2';
 import Register from './auth/Register';
 import UserPage from './auth/UserPage';
 import Discuss from './discuss/Discuss'
@@ -21,9 +20,9 @@ const App = () => (
       <Route exact path="/" component={Splash} />
       <Route exact path="/lessons" component={LessonsList} />
       <Route exact path="/register" component={Register} />
-      {/* <Route exact path="/alphabet" component={Alphabet} /> */}
-      {/* <Route exact path="/matching" component={Matching} />
-      <Route exact path="/advanced" component={Advanced} /> */}
+      <Route exact path="/alphabet" component={Alphabet} />
+      <Route exact path="/matching" component={Matching} />
+      <Route exact path="/advanced" component={Advanced} />
       <Route exact path="/profile" component={UserPage} />
       <Route exact path="/discuss" component={Discuss} />
       <Route exact path="/discuss/:id" component={SingleDiscuss} />
