e7feee4775f4a754ce8c7984c3ce8457c46e71ac
jw-kimm <jwki0071@gmail.com>
2019-09-15 21:13:15 -0400

    renaming components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,9 +4,9 @@ import '../index.css'
 
 import Splash from './splash/Splash'
 import LessonsList from './lessons/LessonsList'
-// import Alphabet from './lessons/Alphabet';
-// import Matching from './lessons/Matching/Matching';
-// import Advanced from './lessons/Advanced/Advanced';
+import Alphabet from './lessons/alphabet/Alphabet';
+import Matching from './lessons/matching/Matching';
+import Advanced from './lessons/advanced/Advanced';
 
 import Register from './auth/Register';
 import UserPage from './auth/UserPage';
@@ -20,9 +20,9 @@ const App = () => (
       <Route exact path="/" component={Splash} />
       <Route exact path="/lessons" component={LessonsList} />
       <Route exact path="/register" component={Register} />
-      {/* <Route exact path="/alphabet" component={Alphabet} />
+      <Route exact path="/alphabet" component={Alphabet} />
       <Route exact path="/matching" component={Matching} />
-      <Route exact path="/advanced" component={Advanced} /> */}
+      <Route exact path="/advanced" component={Advanced} />
       <Route exact path="/profile" component={UserPage} />
       <Route exact path="/discuss" component={Discuss} />
       <Route exact path="/discuss/:id" component={SingleDiscuss} />
